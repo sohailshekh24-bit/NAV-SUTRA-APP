08d9f6ba97958d04dd341c5094eb13923b24e6e0
sohailshekh24-bit <sohail.shekh24@gmail.com>
2026-02-17 00:40:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ try:
     api_key = st.secrets["GEMINI_API_KEY"]
     genai.configure(api_key=api_key)
     # 2026 ka sabse stable engine
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-1.5-flash-latest')
 except:
     st.error("⚠️ Secrets check karein!")
 
